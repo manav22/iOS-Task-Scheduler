ffc9c2e67660f85418c28c8d67bc538de38d5459
Manav Pavitra Singh <manavpavitrasingh@Manavs-MacBook-Pro.local>
2015-07-06 15:43:10 -0700

    Initial Commit

---
+++ Task Scheduler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task Scheduler
+//
+//  Created by Manav Pavitra Singh on 7/6/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task Scheduler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task Scheduler
+//
+//  Created by Manav Pavitra Singh on 7/6/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
